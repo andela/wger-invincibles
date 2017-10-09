bac5425190fa132dfaf6d8ca7e8d38d4941b497d
Muhwezi Allan <allan.muhwezi@andela.com>
2017-10-09 12:40:26 +0300

    fix(css): fix the path to the static css files in bower_components

--- wger/settings_global.py
+++ wger/settings_global.py
@@ -311,7 +311,7 @@ THUMBNAIL_ALIASES = {
 # Django compressor
 #
 STATIC_ROOT = ''
-STATIC_URL = '/static/'
+STATIC_URL = 'wger/core/static/'
 
 # The default is not DEBUG, override if needed
 # COMPRESS_ENABLED = True
