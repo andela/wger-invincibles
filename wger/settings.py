cdfd5360f12409c128100ce6a02fbc70940f312e
Ace <rossie@Roses-MacBook-Pro.local>
2017-10-16 11:56:33 +0300

    [UPDATE]settings files

--- wger/settings.py
+++ wger/settings.py
@@ -16,7 +16,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/Users/kibe/Andela/SIMS/wger/wger-invincibles/wger/database.sqlite',
+        'NAME': 'wger/database.sqlite',
         'USER': '',
         'PASSWORD': '',
         'HOST': '',
@@ -25,7 +25,7 @@ DATABASES = {
 }
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = '9f1dx$xzoefj9!x1x5a8d!f$_p)$&esdzm*g54-ahguz$zh%5m'
+SECRET_KEY = '-jkn(oidb1@v5_#!xur+u*6q&z&yafg8-#p!m2*+&lhmtnoae8'
 
 # Your reCaptcha keys
 RECAPTCHA_PUBLIC_KEY = ''
@@ -39,7 +39,7 @@ SITE_URL = 'http://localhost:8000'
 
 # Path to uploaded files
 # Absolute filesystem path to the directory that will hold user-uploaded files.
-MEDIA_ROOT = '/Users/kibe/.local/share/wger/media'
+MEDIA_ROOT = '/Users/rossie/.local/share/wger/media'
 MEDIA_URL = '/media/'
 
 # Allow all hosts to access the application. Change if used in production.
