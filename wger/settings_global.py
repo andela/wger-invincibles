e0ca50d9ff81d8f550fadd3ec9cc5c337e9910d3
Lornah Lynn Tumuhairwe <lornah.tumuhairwe@andela.com>
2017-10-24 10:10:16 +0300

    ch(circle ci): Editted settings

--- wger/settings_global.py
+++ wger/settings_global.py
@@ -153,6 +153,7 @@ TEMPLATES = [
                 'wger.utils.context_processor.processor',
 
                 # Django
+                'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.template.context_processors.debug',
                 'django.template.context_processors.i18n',
